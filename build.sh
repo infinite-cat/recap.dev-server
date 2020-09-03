d22e9064d460bd068fc6751d8cf584f2e638dbbf
ayankovsky <p44gr10@gmail.com>
2020-09-03 09:34:18 +0200

    Added license, fixed the build script

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ echo "building UI"
 (cd recap.dev-ui && yarn && yarn build)
 echo "building backend"
 (cd recap.dev-backend && yarn && yarn build:prod)
-docker-compose build recap_dev_server
-docker-compose push recap_dev_server
+docker-compose build
+docker-compose push
